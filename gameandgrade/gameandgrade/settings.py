e7913a2e473f56875f810e511516a63de6a75e2f
Frank Minio <miniofrank@gmail.com>
2012-07-17 15:10:23 -0400

    Added a superuser for Eugene

--- gameandgrade/gameandgrade/settings.py
+++ gameandgrade/gameandgrade/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/frank_em/Documents/Aptana Studio 3 Workspace/gameandgrade/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/frank_em/Documents/Aptana Studio 3 Workspace/game-grade/gameandgrade/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
