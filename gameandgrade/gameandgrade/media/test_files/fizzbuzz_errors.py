28fdeec5148f322694eba7439be8cc20632d6be6
Frank Minio <miniofrank@gmail.com>
2012-07-18 14:42:11 -0400

    Lucy's fixed code

---
+++ gameandgrade/gameandgrade/media/test_files/fizzbuzz_errors.py
@@ -0,0 +1,7 @@
+for count in range(1, 31):
+    if count % 3 == 0 and count % 5 == 0:
+        print "FizzBuzz!"
+    elif count % 3 == 0: 
+       print "Fizz!"
+    elif count % 5 == 0: 
+        print "Buzz!"
